2b03a9a36c4197d1ff5840b1b75e8b6719fb15d7
Bishwajit-Bhattacharjee <bishwajit0020@gmail.com>
2020-11-14 02:44:43 +0600

    Test on join added of proj1

--- nachos/proj1/Test.java
+++ nachos/proj1/Test.java
@@ -1,5 +1,10 @@
 package nachos.proj1;
 
+import nachos.machine.Kernel;
+import nachos.machine.Machine;
+import nachos.threads.KThread;
+import nachos.threads.ThreadedKernel;
+
 public class Test {
     public static void test() {
         new JoinTest().performTest();
@@ -10,7 +15,44 @@ public class Test {
 class JoinTest {
     public void performTest() {
 
+        KThread D = new KThread( new JoinTestClass(null, 100000)).setName("D");
+        KThread C = new KThread( new JoinTestClass(D, 0)).setName("C");
+        KThread B = new KThread( new JoinTestClass(C, 0)).setName("B");
+        KThread A = new KThread( new JoinTestClass(B, 0)).setName("A");
+
+        A.fork();
+        B.fork();
+        C.fork();
+        D.fork();
+
+        A.join();
     }
 
+    private static class JoinTestClass implements Runnable {
 
+        JoinTestClass(KThread sleepOn, long sleepTime) {
+            this.sleepOn = sleepOn;
+            this.sleepTime = sleepTime;
+        }
+        @Override
+        public void run() {
+            System.out.println("Thread " + KThread.currentThread().getName()
+                    + " want to sleep on " + ((sleepOn == null)?"None": sleepOn.getName()));
+            System.out.println("Thread " + KThread.currentThread().getName() + " has started");
+
+            if (sleepOn != null) {
+                sleepOn.join();
+            }
+
+            ThreadedKernel.alarm.waitUntil(sleepTime);
+
+            System.out.println("Thread " + KThread.currentThread().getName()
+                    + " has ended at time " + Machine.timer().getTime());
+
+        }
+
+        private long sleepTime;
+        private KThread sleepOn;
+
+    }
 }
